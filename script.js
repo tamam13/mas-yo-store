feb44eed848838874b479f09a1c9c876eb54fc72
tamam13 <muhammadtamam032@gmail.com>
2026-09-08 16:54:19 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById("menuButton");
-const menu = document.getElementById("menu");
-
-menuButton.addEventListener("click", function() {
-    menu.classList.toggle("active");
-});
